6464a91e2607fc1cb5e927f066845c2f58c53a0a
Guillaume Damiens <guillaumedmns@gmail.com>
2022-04-20 00:00:55 +0200

    Fix some styled components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,30 +1,33 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
-import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
-import PhotoCameraOutlinedIcon from "@material-ui/icons/PhotoCameraOutlined";
-import MovieCreationOutlinedIcon from "@material-ui/icons/MovieCreationOutlined";
-import { createStyles, makeStyles, Theme, withTheme } from "@material-ui/core/styles";
-import MenuIcon from "@material-ui/icons/Menu";
-import styled from "styled-components";
-import Typography from "@material-ui/core/Typography";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PhotoCameraOutlinedIcon from "@mui/icons-material/PhotoCameraOutlined";
+import MovieCreationOutlinedIcon from "@mui/icons-material/MovieCreationOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
+import {styled} from "@mui/material";
 
-const Nav = withTheme(styled(Link)`
-  padding: 12px ${(props) => props.theme.spacing(1)}px;
+
+const Nav = styled(Link)(
+    ({ theme }) =>`
+  padding: 12px ${theme.spacing(1)};
   font-size: 18px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-`);
+`,
+);
 
-const NavText = styled.span`
+const NavText = styled('span')(`
   margin: 0px 4px 0px;
-`;
+`
+);
 
-const Main = styled.div``;
-
+const Main = styled('div')(`
+`
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
