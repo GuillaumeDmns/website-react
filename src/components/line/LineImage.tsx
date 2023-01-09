25884c5ab1df018f0ea1939a5c4895dd4e0434d9
Guillaume Damiens <guillaumedmns@gmail.com>
2023-01-09 09:51:18 +0100

    Customize line style

--- src/components/line/LineImage.tsx
+++ src/components/line/LineImage.tsx
@@ -5,10 +5,18 @@ import { styled } from "@mui/material";
 const NavText = styled("div", {
   shouldForwardProp: (prop) => prop !== "line",
 })<{ line: IDFMLine }>(({ line }) => ({
-  backgroundColor: `#${line.lineIdBackgroundColor}`,
-  color: `#${line.lineIdColor}`,
+  backgroundColor: line.transportMode === "TRAM" ? "white" : `#${line.lineIdBackgroundColor}`,
+  color: line.transportMode === "TRAM" ? "black" : `#${line.lineIdColor}`,
+  minWidth: "20px",
+  textAlign: "center",
+  borderRadius: line.transportMode === "METRO" ? "20px" : "0px",
+  borderTop: line.transportMode === "TRAM" ? `5px solid #${line.lineIdBackgroundColor}` : "0px",
+  borderBottom: line.transportMode === "TRAM" ? `5px solid #${line.lineIdBackgroundColor}` : "0px",
+  borderColor: ``,
+  fontWeight: "bold",
   padding: "5px",
   margin: "5px",
+
 }));
 
 type Props = {
